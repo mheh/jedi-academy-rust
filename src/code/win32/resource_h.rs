3d5a67b9baad9f94c24122dd4c7f06763adabd77
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 16:58:25 -0500

    port oracle/code/win32/resource.h

---
+++ src/code/win32/resource_h.rs
@@ -0,0 +1,21 @@
+// {{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by winquake.rc
+//
+pub const IDS_STRING1: i32 = 1;
+pub const IDI_ICON1: i32 = 1;
+pub const IDB_BITMAP1: i32 = 1;
+pub const IDB_BITMAP2: i32 = 128;
+pub const IDC_CURSOR1: i32 = 129;
+
+// Next default values for new objects
+//
+// #ifdef APSTUDIO_INVOKED
+// #ifndef APSTUDIO_READONLY_SYMBOLS
+// #define _APS_NO_MFC                     1
+// #define _APS_NEXT_RESOURCE_VALUE        130
+// #define _APS_NEXT_COMMAND_VALUE         40001
+// #define _APS_NEXT_CONTROL_VALUE         1005
+// #define _APS_NEXT_SYMED_VALUE           101
+// #endif
+// #endif
