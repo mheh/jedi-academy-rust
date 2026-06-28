435705de73ae446258e05f1b256791f015fc9397
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 01:39:55 -0500

    port oracle/codemp/strings/str_server.h

--- src/codemp/strings/mod.rs
+++ src/codemp/strings/mod.rs
@@ -1,1 +1,3 @@
 //! Stub for original `codemp/strings/` sources.
+
+pub mod str_server_h;
---
+++ src/codemp/strings/str_server_h.rs
@@ -0,0 +1,19 @@
+// Original C header: `codemp/strings/str_server.h`
+
+/********************************************************************************
+
+Server strings
+
+********************************************************************************/
+
+/*
+
+// findmeste
+
+#define PACKAGE_STR_SERVER		0x01
+
+enum
+{
+	STR_SERVER_SERVER_NOT_RUNNING = 0x0100
+};
+*/
