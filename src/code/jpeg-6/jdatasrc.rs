ac6e6ebacdd1a1a36bc4a77f4f4e448cf20f8ce0
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 16:58:28 -0500

    port oracle/code/jpeg-6/jdatasrc.cpp

---
+++ src/code/jpeg-6/jdatasrc.rs
@@ -0,0 +1,247 @@
+/*
+ * jdatasrc.c
+ *
+ * Copyright (C) 1994, Thomas G. Lane.
+ * This file is part of the Independent JPEG Group's software.
+ * For conditions of distribution and use, see the accompanying README file.
+ *
+ * This file contains decompression data source routines for the case of
+ * reading JPEG data from a file (or any stdio stream).  While these routines
+ * are sufficient for most applications, some will want to use a different
+ * source manager.
+ * IMPORTANT: we assume that fread() will correctly transcribe an array of
+ * JOCTETs from 8-bit-wide elements on external storage.  If char is wider
+ * than 8 bits on your machine, you may need to do some tweaking.
+ */
+
+// leave this as first line for PCH reasons...
+//
+
+/* this is not a core library module, so it doesn't define JPEG_INTERNALS */
+
+use core::ffi::{c_int, c_void};
+use core::ptr::{self, addr_of, addr_of_mut};
+
+// JPEG library external types - these are declared as opaque since the full
+// definitions are in the JPEG library headers (jinclude.h, jpeglib.h, jerror.h)
+#[repr(C)]
+pub struct j_decompress_struct {
+    _opaque: [u8; 0],
+}
+
+pub type j_decompress_ptr = *mut j_decompress_struct;
+pub type j_common_ptr = *mut j_decompress_struct;
+
+// JPEG type aliases
+pub type JOCTET = u8;
+pub type boolean = c_int;
+
+// JPEG library constants (from jpeglib.h)
+const JPOOL_PERMANENT: i32 = 0;
+
+// Macro replacement for SIZEOF - in C this was a compile-time macro
+#[inline]
+const fn SIZEOF<T>() -> usize {
+    core::mem::size_of::<T>()
+}
+
+// External JPEG library functions
+extern "C" {
+    pub fn jpeg_resync_to_restart(cinfo: j_decompress_ptr, desired: c_int) -> boolean;
+}
+
+// Stub for memcpy - use core::ptr::copy_nonoverlapping
+#[inline]
+unsafe fn memcpy(dst: *mut u8, src: *const u8, size: usize) {
+    core::ptr::copy_nonoverlapping(src, dst, size);
+}
+
+/* Expanded data source object for stdio input */
+
+#[repr(C)]
+pub struct my_source_mgr {
+    pub pub_: jpeg_source_mgr,	/* public fields */
+    pub infile: *mut u8,		/* source stream */
+    pub buffer: *mut JOCTET,		/* start of buffer */
+    pub start_of_file: boolean,	/* have we gotten any data yet? */
+}
+
+pub type my_src_ptr = *mut my_source_mgr;
+
+// We need to declare jpeg_source_mgr fields as they're used in the code
+#[repr(C)]
+pub struct jpeg_source_mgr {
+    pub init_source: Option<extern "C" fn(j_decompress_ptr) -> ()>,
+    pub fill_input_buffer: Option<extern "C" fn(j_decompress_ptr) -> boolean>,
+    pub skip_input_data: Option<extern "C" fn(j_decompress_ptr, c_int) -> ()>,
+    pub resync_to_restart: Option<extern "C" fn(j_decompress_ptr, c_int) -> boolean>,
+    pub term_source: Option<extern "C" fn(j_decompress_ptr) -> ()>,
+    pub bytes_in_buffer: usize,
+    pub next_input_byte: *const JOCTET,
+}
+
+#[allow(non_snake_case)]
+const INPUT_BUF_SIZE: usize = 4096;	/* choose an efficiently fread'able size */
+
+
+/*
+ * Initialize source --- called by jpeg_read_header
+ * before any data is actually read.
+ */
+
+unsafe extern "C" fn init_source(cinfo: j_decompress_ptr) {
+    let src = (*cinfo).src as *mut my_source_mgr;
+
+    /* We reset the empty-input-file flag for each image,
+     * but we don't clear the input buffer.
+     * This is correct behavior for reading a series of images from one source.
+     */
+    (*src).start_of_file = 1;	/* TRUE */
+}
+
+
+/*
+ * Fill the input buffer --- called whenever buffer is emptied.
+ *
+ * In typical applications, this should read fresh data into the buffer
+ * (ignoring the current state of next_input_byte & bytes_in_buffer),
+ * reset the pointer & count to the start of the buffer, and return TRUE
+ * indicating that the buffer has been reloaded.  It is not necessary to
+ * fill the buffer entirely, only to obtain at least one more byte.
+ *
+ * There is no such thing as an EOF return.  If the end of the file has been
+ * reached, the routine has a choice of ERREXIT() or inserting fake data into
+ * the buffer.  In most cases, generating a warning message and inserting a
+ * fake EOI marker is the best course of action --- this will allow the
+ * decompressor to output however much of the image is there.  However,
+ * the resulting error message is misleading if the real problem is an empty
+ * input file, so we handle that case specially.
+ *
+ * In applications that need to be able to suspend compression due to input
+ * not being available yet, a FALSE return indicates that no more data can be
+ * obtained right now, but more may be forthcoming later.  In this situation,
+ * the decompressor will return to its caller (with an indication of the
+ * number of scanlines it has read, if any).  The application should resume
+ * decompression after it has loaded more data into the input buffer.  Note
+ * that there are substantial restrictions on the use of suspension --- see
+ * the documentation.
+ *
+ * When suspending, the decompressor will back up to a convenient restart point
+ * (typically the start of the current MCU). next_input_byte & bytes_in_buffer
+ * indicate where the restart point will be if the current call returns FALSE.
+ * Data beyond this point must be rescanned after resumption, so move it to
+ * the front of the buffer rather than discarding it.
+ */
+
+unsafe extern "C" fn fill_input_buffer(cinfo: j_decompress_ptr) -> boolean {
+    let src = (*cinfo).src as *mut my_source_mgr;
+
+    memcpy((*src).buffer, (*src).infile, INPUT_BUF_SIZE);
+
+    (*src).infile = (*src).infile.add(INPUT_BUF_SIZE);
+
+    (*(*cinfo).src).next_input_byte = (*src).buffer as *const JOCTET;
+    (*(*cinfo).src).bytes_in_buffer = INPUT_BUF_SIZE;
+    (*src).start_of_file = 0;	/* FALSE */
+
+    1	/* TRUE */
+}
+
+
+/*
+ * Skip data --- used to skip over a potentially large amount of
+ * uninteresting data (such as an APPn marker).
+ *
+ * Writers of suspendable-input applications must note that skip_input_data
+ * is not granted the right to give a suspension return.  If the skip extends
+ * beyond the data currently in the buffer, the buffer can be marked empty so
+ * that the next read will cause a fill_input_buffer call that can suspend.
+ * Arranging for additional bytes to be discarded before reloading the input
+ * buffer is the application writer's problem.
+ */
+
+unsafe extern "C" fn skip_input_data(cinfo: j_decompress_ptr, mut num_bytes: c_int) {
+    let src = (*cinfo).src as *mut my_source_mgr;
+
+    /* Just a dumb implementation for now.  Could use fseek() except
+     * it doesn't work on pipes.  Not clear that being smart is worth
+     * any trouble anyway --- large skips are infrequent.
+     */
+    if num_bytes > 0 {
+        while num_bytes > (*(*cinfo).src).bytes_in_buffer as c_int {
+            num_bytes -= (*(*cinfo).src).bytes_in_buffer as c_int;
+            fill_input_buffer(cinfo);
+            /* note we assume that fill_input_buffer will never return FALSE,
+             * so suspension need not be handled.
+             */
+        }
+        (*(*cinfo).src).next_input_byte = ((*(*cinfo).src).next_input_byte as *const u8)
+            .add(num_bytes as usize) as *const JOCTET;
+        (*(*cinfo).src).bytes_in_buffer -= num_bytes as usize;
+    }
+}
+
+
+/*
+ * An additional method that can be provided by data source modules is the
+ * resync_to_restart method for error recovery in the presence of RST markers.
+ * For the moment, this source module just uses the default resync method
+ * provided by the JPEG library.  That method assumes that no backtracking
+ * is possible.
+ */
+
+
+/*
+ * Terminate source --- called by jpeg_finish_decompress
+ * after all data has been read.  Often a no-op.
+ *
+ * NB: *not* called by jpeg_abort or jpeg_destroy; surrounding
+ * application must deal with any cleanup that should happen even
+ * for error exit.
+ */
+
+unsafe extern "C" fn term_source(cinfo: j_decompress_ptr) {
+    /* no work necessary here */
+}
+
+
+/*
+ * Prepare for input from a stdio stream.
+ * The caller must have already opened the stream, and is responsible
+ * for closing it after finishing decompression.
+ */
+
+pub unsafe extern "C" fn jpeg_stdio_src(cinfo: j_decompress_ptr, infile: *mut u8) {
+    let mut src: my_src_ptr;
+
+    /* The source object and input buffer are made permanent so that a series
+     * of JPEG images can be read from the same file by calling jpeg_stdio_src
+     * only before the first one.  (If we discarded the buffer at the end of
+     * one image, we'd likely lose the start of the next one.)
+     * This makes it unsafe to use this manager and a different source
+     * manager serially with the same JPEG object.  Caveat programmer.
+     */
+    if (*cinfo).src.is_null() {	/* first time for this JPEG object? */
+        (*cinfo).src = (*(*cinfo).mem).alloc_small.unwrap()(
+            cinfo as j_common_ptr,
+            JPOOL_PERMANENT,
+            SIZEOF::<my_source_mgr>(),
+        ) as *mut jpeg_source_mgr;
+        src = (*cinfo).src as *mut my_source_mgr;
+        (*src).buffer = (*(*cinfo).mem).alloc_small.unwrap()(
+            cinfo as j_common_ptr,
+            JPOOL_PERMANENT,
+            INPUT_BUF_SIZE * SIZEOF::<JOCTET>(),
+        ) as *mut JOCTET;
+    }
+
+    src = (*cinfo).src as *mut my_source_mgr;
+    (*(*cinfo).src).init_source = Some(init_source);
+    (*(*cinfo).src).fill_input_buffer = Some(fill_input_buffer);
+    (*(*cinfo).src).skip_input_data = Some(skip_input_data);
+    (*(*cinfo).src).resync_to_restart = Some(jpeg_resync_to_restart); /* use default method */
+    (*(*cinfo).src).term_source = Some(term_source);
+    (*src).infile = infile;
+    (*(*cinfo).src).bytes_in_buffer = 0; /* forces fill_input_buffer on first read */
+    (*(*cinfo).src).next_input_byte = ptr::null(); /* until buffer loaded */
+}
