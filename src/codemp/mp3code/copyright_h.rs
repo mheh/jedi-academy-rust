ebab31ad21bd7ca82135cef36e2fe01e9b13ade4
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 02:24:08 -0500

    port oracle/codemp/mp3code/copyright.h

---
+++ src/codemp/mp3code/copyright_h.rs
@@ -0,0 +1,23 @@
+//! Mechanical port of `codemp/mp3code/copyright.h`.
+
+#![allow(non_upper_case_globals)]
+
+//############################################################################
+//##                                                                        ##
+//##  MSS 4.0 Miles Sound Studio                                            ##
+//##                                                                        ##
+//##  V1.00 of 18-Mar-96: Initial version                                   ##
+//##                                                                        ##
+//##  C source compatible with Microsoft C v9.0 or later                    ##
+//##                                                                        ##
+//##   Author: Jeff Roberts                                                 ##
+//##                                                                        ##
+//############################################################################
+//##                                                                        ##
+//##  Copyright (C) RAD Game Tools, Inc.                                    ##
+//##                                                                        ##
+//##  For technical support, contact RAD Game Tools at 425-893-4300.        ##
+//##                                                                        ##
+//############################################################################
+
+pub const MSS_COPYRIGHT: &str = "Copyright (C) 1991-2000, RAD Game Tools, Inc.";
--- src/codemp/mp3code/mod.rs
+++ src/codemp/mp3code/mod.rs
@@ -1,3 +1,4 @@
 //! Stub for original `codemp/mp3code/` sources.
 
+pub mod copyright_h;
 pub mod small_header_h;
