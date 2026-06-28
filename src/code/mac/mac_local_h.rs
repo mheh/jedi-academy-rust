f3fc58768b1241b9e42888e0f8842a6c3ece0296
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 16:58:29 -0500

    port oracle/code/mac/mac_local.h

---
+++ src/code/mac/mac_local_h.rs
@@ -0,0 +1,329 @@
+/*
+ *	Apple Universal Headers 3.1
+ *
+ *	Uncomment any additional #includes you want to add to your MacHeaders.
+ */
+// #include <MacHeaders.h>
+
+// #include <ADSP.h>
+// #include <ADSPSecure.h>
+// #include <AEDataModel.h>
+// #include <AEObjects.h>
+// #include <AEPackObject.h>
+// #include <AERegistry.h>
+// #include <AEUserTermTypes.h>
+// #include <AIFF.h>
+// #include <Aliases.h>
+// #include <Appearance.h>
+// #include <AppleEvents.h>
+// #include <AppleGuide.h>
+// #include <AppleScript.h>
+// #include <AppleTalk.h>
+// #include <ASDebugging.h>
+// #include <ASRegistry.h>
+// #include <ATA.h>
+// #include <AVComponents.h>
+// #include <Balloons.h>
+// #include <CardServices.h>
+// #include <CMAcceleration.h>
+// #include <CMApplication.h>
+// #include <CMCalibrator.h>
+// #include <CMComponent.h>
+// #include <CMConversions.h>
+// #include <CMICCProfile.h>
+// #include <CMMComponent.h>
+// #include <CMPRComponent.h>
+// #include <CMScriptingPlugin.h>
+// #include <CodeFragments.h>
+// #include <Collections.h>
+// #include <ColorPicker.h>
+// #include <ColorPickerComponents.h>
+// #include <CommResources.h>
+// #include <Components.h>
+// #include <ConditionalMacros.h>
+// #include <Connections.h>
+// #include <ConnectionTools.h>
+// #include <Controls.h>
+// #include <ControlStrip.h>
+// #include <CRMSerialDevices.h>
+// #include <CTBUtilities.h>
+// #include <CursorDevices.h>
+// #include <DatabaseAccess.h>
+// #include <DateTimeUtils.h>
+// #include <Desk.h>
+// #include <DeskBus.h>
+// #include <Devices.h>
+// #include <Dialogs.h>
+// #include <Dictionary.h>
+// #include <DigitalSignature.h>
+// #include <DiskInit.h>
+// #include <Disks.h>
+// #include <Displays.h>
+// #include <Drag.h>
+// #include <DrawSprocket.h>
+// #include <DriverSynchronization.h>
+// #include <DriverFamilyMatching.h>
+// #include <DriverGestalt.h>
+// #include <DriverServices.h>
+// #include <DriverSupport.h>
+// #include <Editions.h>
+// #include <Endian.h>
+// #include <ENET.h>
+// #include <EPPC.h>
+// #include <Errors.h>
+// #include <Events.h>
+// #include <fenv.h>
+// #include <Files.h>
+// #include <FileTransfers.h>
+// #include <FileTransferTools.h>
+// #include <FileTypesAndCreators.h>
+// #include <Finder.h>
+// #include <FinderRegistry.h>
+// #include <FixMath.h>
+// #include <Folders.h>
+// #include <Fonts.h>
+// #include <fp.h>
+// #include <FragLoad.h>
+// #include <FSM.h>
+// #include <Gestalt.h>
+// #include <GXEnvironment.h>
+// #include <GXErrors.h>
+// #include <GXFonts.h>
+// #include <GXGraphics.h>
+// #include <GXLayout.h>
+// #include <GXMath.h>
+// #include <GXMessages.h>
+// #include <GXPrinterDrivers.h>
+// #include <GXPrinting.h>
+// #include <GXTypes.h>
+// #include <GoggleSprocket.h>
+// #include <HFSVolumes.h>
+// #include <HyperXCmd.h>
+// #include <Icons.h>
+// #include <ImageCodec.h>
+// #include <ImageCompression.h>
+// #include <InputSprocket.h>
+// #include <Interrupts.h>
+// #include <IntlResources.h>
+// #include <JManager.h>
+// #include <Kernel.h>
+// #include <Language.h>
+// #include <Lists.h>
+// #include <LocationManager.h>
+// #include <LocationManager.k.h>
+// #include <LowMem.h>
+// #include <MachineExceptions.h>
+// #include <MacMemory.h>
+// #include <MacTCP.h>
+// #include <MacTypes.h>
+// #include <MacWindows.h>
+// #include <Math64.h>
+// #include <MediaHandlers.h>
+// #include <Memory.h>
+// #include <Menus.h>
+// #include <MIDI.h>
+// #include <MixedMode.h>
+// #include <Movies.h>
+// #include <MoviesFormat.h>
+// #include <MP.h>
+// #include <Multiprocessing.h>
+// #include <NameRegistry.h>
+// #include <NetSprocket.h>
+// #include <Notification.h>
+// #include <NumberFormatting.h>
+// #include <OCE.h>
+// #include <OCEAuthDir.h>
+// #include <OCEErrors.h>
+// #include <OCEMail.h>
+// #include <OCEMessaging.h>
+// #include <OCEStandardDirectory.h>
+// #include <OCEStandardMail.h>
+// #include <OCETemplates.h>
+// #include <OSA.h>
+// #include <OSAComp.h>
+// #include <OSAGeneric.h>
+// #include <OSUtils.h>
+// #include <Packages.h>
+// #include <Palettes.h>
+// #include <Patches.h>
+// #include <PCCardAdapterPlugin.h>
+// #include <PCCardEnablerPlugin.h>
+// #include <PCCardTuples.h>
+// #include <PCI.h>
+// #include <PEFBinaryFormat.h>
+// #include <PCCard.h>
+// #include <Picker.h>
+// #include <PictUtil.h>
+// #include <PictUtils.h>
+// #include <PLStringFuncs.h>
+// #include <Power.h>
+// #include <PPCToolbox.h>
+// #include <Printing.h>
+// #include <Processes.h>
+// #include <QD3DAcceleration.h>
+// #include <QD3DCamera.h>
+// #include <QD3DController.h>
+// #include <QD3DCustomElements.h>
+// #include <QD3DDrawContext.h>
+// #include <QD3DErrors.h>
+// #include <QD3DExtension.h>
+// #include <QD3DGeometry.h>
+// #include <QD3DGroup.h>
+// #include <QD3DIO.h>
+// #include <QD3DLight.h>
+// #include <QD3DMath.h>
+// #include <QD3DPick.h>
+// #include <QD3DRenderer.h>
+// #include <QD3DSet.h>
+// #include <QD3DShader.h>
+// #include <QD3DStorage.h>
+// #include <QD3DString.h>
+// #include <QD3DStyle.h>
+// #include <QD3DTransform.h>
+// #include <QD3DView.h>
+// #include <QD3DViewer.h>
+// #include <QD3DWinViewer.h>
+// #include <QD3D.h>
+// #include <QDOffscreen.h>
+// #include <QTML.h>
+// #include <QTMLCustomElements.h>
+// #include <Quickdraw.h>
+// #include <QuickdrawText.h>
+// #include <QuickTimeComponents.h>
+// #include <QuickTimeVR.h>
+// #include <QuickTimeVRFormat.h>
+// #include <RAVE.h>
+// #include <QuickTimeMusic.h>
+// #include <Resources.h>
+// #include <Retrace.h>
+// #include <ROMDefs.h>
+// #include <ScalerTypes.h>
+// #include <ScalerStreamTypes.h>
+// #include <Scrap.h>
+// #include <Script.h>
+// #include <SCSI.h>
+// #include <SegLoad.h>
+// #include <Serial.h>
+// #include <SFNTTypes.h>
+// #include <SFNTLayoutTypes.h>
+// #include <ShutDown.h>
+// #include <SIOW.h>
+// #include <Slots.h>
+// #include <SocketServices.h>
+// #include <Sound.h>
+// #include <SoundComponents.h>
+// #include <SoundInput.h>
+// #include <SoundSprocket.h>
+// #include <Speech.h>
+// #include <SpeechRecognition.h>
+// #include <SpeechSynthesis.h>
+// #include <StandardFile.h>
+// #include <StringCompare.h>
+// #include <Start.h>
+// #include <Strings.h>
+// #include <Telephones.h>
+// #include <Terminals.h>
+// #include <TerminalTools.h>
+// #include <TextCommon.h>
+// #include <TextEdit.h>
+// #include <TextEncodingConverter.h>
+// #include <TextServices.h>
+// #include <TextUtils.h>
+// #include <Threads.h>
+// #include <Timer.h>
+// #include <ToolUtils.h>
+// #include <Translation.h>
+// #include <TranslationExtensions.h>
+// #include <Traps.h>
+// #include <TSMTE.h>
+// #include <Types.h>
+// #include <Unicode.h>
+// #include <UnicodeConverter.h>
+// #include <Video.h>
+// #include <VideoServices.h>
+// #include <Windows.h>
+// #include <WorldScript.h>
+// #include <ZoomedVideo.h>
+
+// #include <gl.h>
+// #include <agl.h>
+// #include <console.h>
+// #include <SIOUX.h>
+// #include <math.h>
+// #include <time.h>
+// #include <errno.h>
+// #include <Profiler.h>
+
+use crate::code::qcommon::q_shared::{cvar_t, qboolean};
+use core::ffi::{c_char, c_int, c_void};
+
+/* Menus: */
+pub const rMenuBar: c_int = 128;
+/* Apple menu: */
+pub const mApple: c_int = 128;
+pub const iAbout: c_int = 1;
+/* File menu: */
+pub const mFile: c_int = 129;
+pub const iQuit: c_int = 1;
+/* Edit menu: */
+pub const mEdit: c_int = 130;
+pub const iUndo: c_int = 1;
+pub const iCut: c_int = 3;
+pub const iCopy: c_int = 4;
+pub const iPaste: c_int = 5;
+pub const iClear: c_int = 6;
+/* Windows: */
+pub const kMainWindow: c_int = 128;
+pub const kFullScreenWindow: c_int = 129;
+/* Dilogs: */
+pub const kAboutDialog: c_int = 128;
+
+// mac_main.c
+pub static mut sys_ticBase: c_int = 0;
+pub static mut sys_msecBase: c_int = 0;
+pub static mut sys_lastEventTic: c_int = 0;
+
+pub static mut sys_waitNextEvent: *mut cvar_t = std::ptr::null_mut();
+
+extern "C" {
+	// From q_shared or other modules
+	pub fn Sys_QueEvent(time: c_int, r#type: u32, value: c_int, value2: c_int, ptrLength: c_int, ptr: *mut c_void);
+	pub fn PStringToCString(s: *mut c_char) -> c_int;
+	pub fn CStringToPString(s: *mut c_char) -> c_int;
+}
+
+// mac_event.c
+extern "C" {
+	pub static vkeyToQuakeKey: [c_int; 256];
+	pub fn Sys_SendKeyEvents();
+}
+
+// mac_net.c
+extern "C" {
+	pub fn Sys_InitNetworking();
+	pub fn Sys_ShutdownNetworking();
+}
+
+// mac_input.c
+extern "C" {
+	pub fn Sys_InitInput();
+	pub fn Sys_ShutdownInput();
+	pub fn Sys_Input();
+}
+
+pub static mut inputActive: qboolean = 0;
+
+// mac_glimp.c
+extern "C" {
+	pub static glConfig: crate::code::qcommon::qcommon::glconfig_t;
+}
+
+// mac_console.c
+
+extern "C" {
+	pub fn Sys_InitConsole();
+	pub fn Sys_ShowConsole(level: c_int, quitOnClose: qboolean);
+	pub fn Sys_Print(text: *const c_char);
+	pub fn Sys_ConsoleInput() -> *mut c_char;
+	pub fn Sys_ConsoleEvent(event: *mut c_void) -> qboolean;
+}
