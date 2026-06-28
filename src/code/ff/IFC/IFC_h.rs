9f740d511b36f32dd9b5fd18889c80da10e821a9
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 16:58:26 -0500

    port oracle/code/ff/IFC/IFC.h

---
+++ src/code/ff/IFC/IFC_h.rs
@@ -0,0 +1,74 @@
+/**********************************************************************
+	Copyright (c) 1997 - 2000 Immersion Corporation
+
+	Permission to use, copy, modify, distribute, and sell this
+	software and its documentation may be granted without fee;
+	interested parties are encouraged to request permission from
+		Immersion Corporation
+		801 Fox Lane
+		San Jose, CA 95131
+		408-467-1900
+
+	IMMERSION DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+	INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+	IN NO EVENT SHALL IMMERSION BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+	CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+	LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+	NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+	CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+  FILE:		IFC.h
+
+  PURPOSE:	Class Types for the Force Foundation Classes
+
+  STARTED:	10/29/97
+
+  NOTES/REVISIONS:
+     3/2/99 jrm (Jeff Mallett): Force-->Feel renaming
+
+**********************************************************************/
+
+// Porting note: AFX_IFC_H__135B88C4_4175_11D1_B049_0020AF30269A__INCLUDED_
+// Header guard translated from preprocessor to module system
+
+pub mod ImmBox_h;
+pub mod ImmCondition_h;
+pub mod ImmConstant_h;
+pub mod ImmDamper_h;
+pub mod ImmDevice_h;
+pub mod ImmDevices_h;
+pub mod ImmDXDevice_h;
+pub mod ImmEffect_h;
+pub mod ImmEllipse_h;
+pub mod ImmEnclosure_h;
+pub mod ImmMouse_h;
+pub mod ImmFriction_h;
+pub mod ImmGrid_h;
+pub mod ImmInertia_h;
+pub mod ImmPeriodic_h;
+pub mod ImmProjects_h;
+pub mod ImmRamp_h;
+pub mod ImmSpring_h;
+pub mod ImmTexture_h;
+pub mod IFCErrors_h;
+
+pub use ImmBox_h::*;
+pub use ImmCondition_h::*;
+pub use ImmConstant_h::*;
+pub use ImmDamper_h::*;
+pub use ImmDevice_h::*;
+pub use ImmDevices_h::*;
+pub use ImmDXDevice_h::*;
+pub use ImmEffect_h::*;
+pub use ImmEllipse_h::*;
+pub use ImmEnclosure_h::*;
+pub use ImmMouse_h::*;
+pub use ImmFriction_h::*;
+pub use ImmGrid_h::*;
+pub use ImmInertia_h::*;
+pub use ImmPeriodic_h::*;
+pub use ImmProjects_h::*;
+pub use ImmRamp_h::*;
+pub use ImmSpring_h::*;
+pub use ImmTexture_h::*;
+pub use IFCErrors_h::*;
