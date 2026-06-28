8da94085c09d13b5e3f0c006ce03d0721f7e5373
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 10:56:12 -0500

    port oracle/codemp/encryption/encryption.h

---
+++ src/codemp/encryption/encryption_h.rs
@@ -0,0 +1,9 @@
+//! `encryption.h` — Pakinator main header.
+
+// Pakinator Main Header
+
+// define this to enable all pak files encryption requirements
+// #ifdef FINAL_BUILD
+// //#define _ENCRYPTION_
+// #endif
+
--- src/codemp/encryption/mod.rs
+++ src/codemp/encryption/mod.rs
@@ -1,1 +1,3 @@
 //! Stub for original `codemp/encryption/` sources.
+
+pub mod encryption_h;
