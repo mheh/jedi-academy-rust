e76407e842881702f7433b055fd9161489516fdb
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 14:31:32 -0500

    port oracle/code/game/g_headers.cpp

---
+++ src/code/game/g_headers.rs
@@ -0,0 +1,17 @@
+// Precompiled header file for the game dll
+
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(dead_code)]
+
+// PORTING: g_headers.h is unported
+// use crate::code::game::g_headers_h;
+
+// PORTING: npc_headers.h is unported (NPC_Headers.h in original)
+// use crate::code::game::npc_headers_h;
+
+// PORTING: cg_headers is unported
+// use crate::code::cgame::cg_headers;
+
+// end
