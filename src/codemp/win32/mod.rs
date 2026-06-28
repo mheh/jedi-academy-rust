1e295cc4726df4d2197ec62abc2d5fb29ea7c9fb
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 01:38:04 -0500

    port oracle/codemp/win32/resource.h

--- src/codemp/win32/mod.rs
+++ src/codemp/win32/mod.rs
@@ -1,1 +1,3 @@
 //! Stub for original `codemp/win32/` sources.
+
+pub mod resource_h;
---
+++ src/codemp/win32/resource_h.rs
@@ -0,0 +1,15 @@
+#![allow(non_snake_case)]
+
+pub const IDS_STRING1: i32 = 1;
+pub const IDI_ICON1: i32 = 1;
+pub const IDB_BITMAP1: i32 = 1;
+pub const IDB_BITMAP2: i32 = 128;
+pub const IDC_CURSOR1: i32 = 129;
+pub const IDC_CURSOR2: i32 = 130;
+pub const IDC_CURSOR3: i32 = 131;
+
+pub const _APS_NO_MFC: i32 = 1;
+pub const _APS_NEXT_RESOURCE_VALUE: i32 = 132;
+pub const _APS_NEXT_COMMAND_VALUE: i32 = 40001;
+pub const _APS_NEXT_CONTROL_VALUE: i32 = 1005;
+pub const _APS_NEXT_SYMED_VALUE: i32 = 101;
