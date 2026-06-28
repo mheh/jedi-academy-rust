3f0ef43fe9ba2ef5d10d49e32c1b7f2b4016d5cd
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 16:58:25 -0500

    port oracle/codemp/jpeg-6/jversion.h

---
+++ src/codemp/jpeg-6/jversion_h.rs
@@ -0,0 +1,13 @@
+/*
+ * jversion.h
+ *
+ * Copyright (C) 1991-1995, Thomas G. Lane.
+ * This file is part of the Independent JPEG Group's software.
+ * For conditions of distribution and use, see the accompanying README file.
+ *
+ * This file contains software version identification.
+ */
+
+pub const JVERSION: &str = "6  2-Aug-95";
+
+pub const JCOPYRIGHT: &str = "Copyright (C) 1995, Thomas G. Lane";
