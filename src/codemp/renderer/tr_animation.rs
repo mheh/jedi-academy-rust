79391c99b673d50fd3c115b89c737417b1c00b43
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 16:58:25 -0500

    port oracle/codemp/renderer/tr_animation.cpp

---
+++ src/codemp/renderer/tr_animation.rs
@@ -0,0 +1,8 @@
+#![allow(non_snake_case)]
+
+// All bones should be an identity orientation to display the mesh exactly
+// as it is specified.
+//
+// For all other frames, the bones represent the transformation from the
+// orientation of the bone in the base frame to the orientation in this
+// frame.
