f0209b9681380a9d91492195f971609ae8137e7e
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 02:24:47 -0500

    port oracle/codemp/mp3code/port.h

--- src/codemp/mp3code/mod.rs
+++ src/codemp/mp3code/mod.rs
@@ -3,4 +3,5 @@
 pub mod config_h;
 pub mod copyright_h;
 pub mod jdw_h;
+pub mod port_h;
 pub mod small_header_h;
---
+++ src/codemp/mp3code/port_h.rs
@@ -0,0 +1,73 @@
+/*____________________________________________________________________________
+
+    FreeAmp - The Free MP3 Player
+
+        MP3 Decoder originally Copyright (C) 1995-1997 Xing Technology
+        Corp.  http://www.xingtech.com
+
+    Portions Copyright (C) 1998-1999 EMusic.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    $Id: port.h,v 1.2 1999/10/19 07:13:08 elrod Exp $
+____________________________________________________________________________*/
+
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
+use core::ffi::{c_int, c_uchar, c_uint};
+
+pub const O_BINARY: c_int = 0;
+
+/*--- no kb function unless DOS ---*/
+pub const KB_OK: bool = cfg!(any(target_os = "windows", target_os = "dos"));
+
+/*-- no pcm conversion to wave required
+ if short = 16 bits and little endian ---*/
+
+/* mods 1/9/97 LITTLE_SHORT16 detect */
+
+// Rust has target-endian cfg but no direct `__MSDOS__`, `_CONSOLE`, or `_M_IX86`
+// equivalents. The semantic condition from the C comment is the compatibility
+// boundary: 16-bit `short` data on little-endian targets needs no byte conversion.
+#[cfg(target_endian = "little")]
+pub const LITTLE_SHORT16: bool = true;
+#[cfg(not(target_endian = "little"))]
+pub const LITTLE_SHORT16: bool = false;
+
+// JDW //
+//#ifdef LITTLE_SHORT16
+//#define cvt_to_wave_init(a)
+//#define cvt_to_wave(a, b)  b
+//#else
+//void cvt_to_wave_init(int bits);
+//unsigned int cvt_to_wave(void *a, unsigned int b);
+//
+//#endif
+#[cfg(target_endian = "little")]
+#[inline]
+pub fn cvt_to_wave_init(_bits: c_int) {}
+
+#[cfg(target_endian = "little")]
+#[inline]
+pub unsafe fn cvt_to_wave(_a: *mut c_uchar, b: c_uint) -> c_uint {
+    b
+}
+
+#[cfg(not(target_endian = "little"))]
+extern "C" {
+    pub fn cvt_to_wave_init(bits: c_int);
+    pub fn cvt_to_wave(a: *mut c_uchar, b: c_uint) -> c_uint;
+}
