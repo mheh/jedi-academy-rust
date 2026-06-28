e36bbfd060b929123dadc6edd635fabb92cabc23
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 14:31:33 -0500

    port oracle/codemp/jpeg-6/jcapimin.cpp

---
+++ src/codemp/jpeg-6/jcapimin.rs
@@ -0,0 +1,376 @@
+/*
+ * jcapimin.c
+ *
+ * Copyright (C) 1994-1995, Thomas G. Lane.
+ * This file is part of the Independent JPEG Group's software.
+ * For conditions of distribution and use, see the accompanying README file.
+ *
+ * This file contains application interface code for the compression half
+ * of the JPEG library.  These are the "minimum" API routines that may be
+ * needed in either the normal full-compression case or the transcoding-only
+ * case.
+ *
+ * Most of the routines intended to be called directly by an application
+ * are in this file or in jcapistd.c.  But also see jcparam.c for
+ * parameter-setup helper routines, jcomapi.c for routines shared by
+ * compression and decompression, and jctrans.c for the transcoding case.
+ */
+
+use core::ffi::{c_int, c_void};
+
+// ============================================================================
+// Stubs for JPEG-6 types and structures needed for structural coherence
+// ============================================================================
+
+pub type JDIMENSION = u32;
+pub type JOCTET = u8;
+pub type JSAMPROW = *mut u8;
+pub type JSAMPARRAY = *mut JSAMPROW;
+pub type JSAMPIMAGE = *mut JSAMPARRAY;
+pub type boolean = u8;
+
+// Constants from jpeglib.h and jpegint.h
+const NUM_QUANT_TBLS: usize = 4;   /* Quantization tables are numbered 0..3 */
+const NUM_HUFF_TBLS: usize = 4;    /* Huffman tables are numbered 0..3 */
+const CSTATE_START: c_int = 100;   /* after create_compress */
+const CSTATE_SCANNING: c_int = 101;/* start_compress done, write_scanlines OK */
+const CSTATE_RAW_OK: c_int = 102;  /* start_compress done, write_raw_data OK */
+const CSTATE_WRCOEFS: c_int = 103; /* jpeg_write_coefficients done */
+
+const FALSE: boolean = 0;
+const TRUE: boolean = 1;
+
+const JPOOL_PERMANENT: c_int = 0;  /* lasts until master record is destroyed */
+
+const JERR_TOO_LITTLE_DATA: c_int = 1;
+const JERR_BAD_STATE: c_int = 2;
+const JERR_CANT_SUSPEND: c_int = 3;
+
+// Forward declarations of structures
+#[repr(C)]
+pub struct JQUANT_TBL {
+    pub quantval: [u16; 64],       /* quantization step for each coefficient */
+    pub sent_table: boolean,       /* TRUE when table has been output */
+}
+
+#[repr(C)]
+pub struct JHUFF_TBL {
+    pub bits: [u8; 17],            /* bits[k] = # of symbols with codes of length k bits; bits[0] is unused */
+    pub huffval: [u8; 256],        /* The symbols, in order of incr code length */
+    pub sent_table: boolean,       /* TRUE when table has been output */
+}
+
+#[repr(C)]
+pub struct jpeg_error_mgr {
+    pub reset_error_mgr: Option<unsafe extern "C" fn(*mut c_void)>,
+}
+
+#[repr(C)]
+pub struct jpeg_memory_mgr {
+    pub alloc_small: Option<unsafe extern "C" fn(*mut c_void, c_int, usize) -> *mut c_void>,
+}
+
+#[repr(C)]
+pub struct jpeg_destination_mgr {
+    pub init_destination: Option<unsafe extern "C" fn(*mut c_void)>,
+    pub term_destination: Option<unsafe extern "C" fn(*mut c_void)>,
+}
+
+#[repr(C)]
+pub struct jpeg_progress_mgr {
+    pub pass_counter: core::ffi::c_long,
+    pub pass_limit: core::ffi::c_long,
+    pub progress_monitor: Option<unsafe extern "C" fn(*mut c_void)>,
+}
+
+#[repr(C)]
+pub struct jpeg_marker_writer {
+    pub finish_pass: Option<unsafe extern "C" fn(*mut c_void)>,
+    pub prepare_for_pass: Option<unsafe extern "C" fn(*mut c_void)>,
+    pub is_last_pass: boolean,
+    pub write_file_trailer: Option<unsafe extern "C" fn(*mut c_void)>,
+    pub write_any_marker: Option<unsafe extern "C" fn(*mut c_void, c_int, *const JOCTET, core::ffi::c_uint)>,
+    pub write_tables_only: Option<unsafe extern "C" fn(*mut c_void)>,
+}
+
+#[repr(C)]
+pub struct jpeg_coef_controller {
+    pub compress_data: Option<unsafe extern "C" fn(*mut c_void, *mut JSAMPIMAGE) -> boolean>,
+}
+
+#[repr(C)]
+pub struct j_compress_struct {
+    pub err: *mut jpeg_error_mgr,
+    pub mem: *mut jpeg_memory_mgr,
+    pub progress: *mut jpeg_progress_mgr,
+    pub is_decompressor: boolean,
+    pub global_state: c_int,
+    pub dest: *mut jpeg_destination_mgr,
+    pub comp_info: *mut c_void,
+    pub quant_tbl_ptrs: [*mut JQUANT_TBL; NUM_QUANT_TBLS],
+    pub dc_huff_tbl_ptrs: [*mut JHUFF_TBL; NUM_HUFF_TBLS],
+    pub ac_huff_tbl_ptrs: [*mut JHUFF_TBL; NUM_HUFF_TBLS],
+    pub input_gamma: f64,
+    pub next_scanline: JDIMENSION,
+    pub image_height: JDIMENSION,
+    pub total_iMCU_rows: JDIMENSION,
+    pub master: *mut jpeg_marker_writer,
+    pub coef: *mut jpeg_coef_controller,
+    pub marker: *mut jpeg_marker_writer,
+}
+
+pub type j_compress_ptr = *mut j_compress_struct;
+pub type j_common_ptr = *mut j_compress_struct;
+
+// External function declarations
+extern "C" {
+    pub fn jinit_memory_mgr(cinfo: j_common_ptr);
+    pub fn jpeg_destroy(cinfo: j_common_ptr);
+    pub fn jpeg_abort(cinfo: j_common_ptr);
+    pub fn jinit_marker_writer(cinfo: j_compress_ptr);
+}
+
+// MEMZERO macro equivalent
+#[inline]
+unsafe fn MEMZERO(target: *mut c_void, size: usize) {
+    core::ptr::write_bytes(target as *mut u8, 0, size);
+}
+
+// SIZEOF macro equivalent
+#[inline]
+const fn SIZEOF<T>() -> usize {
+    core::mem::size_of::<T>()
+}
+
+/*
+ * Initialization of a JPEG compression object.
+ * The error manager must already be set up (in case memory manager fails).
+ */
+
+pub unsafe fn jpeg_create_compress(cinfo: j_compress_ptr) {
+    let i: c_int;
+
+    /* For debugging purposes, zero the whole master structure.
+     * But error manager pointer is already there, so save and restore it.
+     */
+    let err = (*cinfo).err;
+    MEMZERO(cinfo as *mut c_void, SIZEOF::<j_compress_struct>());
+    (*cinfo).err = err;
+
+    (*cinfo).is_decompressor = FALSE;
+
+    /* Initialize a memory manager instance for this object */
+    jinit_memory_mgr(cinfo as j_common_ptr);
+
+    /* Zero out pointers to permanent structures. */
+    (*cinfo).progress = core::ptr::null_mut();
+    (*cinfo).dest = core::ptr::null_mut();
+
+    (*cinfo).comp_info = core::ptr::null_mut();
+
+    for i in 0..NUM_QUANT_TBLS as c_int {
+        (*cinfo).quant_tbl_ptrs[i as usize] = core::ptr::null_mut();
+    }
+
+    for i in 0..NUM_HUFF_TBLS as c_int {
+        (*cinfo).dc_huff_tbl_ptrs[i as usize] = core::ptr::null_mut();
+        (*cinfo).ac_huff_tbl_ptrs[i as usize] = core::ptr::null_mut();
+    }
+
+    (*cinfo).input_gamma = 1.0;    /* in case application forgets */
+
+    /* OK, I'm ready */
+    (*cinfo).global_state = CSTATE_START;
+}
+
+
+/*
+ * Destruction of a JPEG compression object
+ */
+
+pub unsafe fn jpeg_destroy_compress(cinfo: j_compress_ptr) {
+    jpeg_destroy(cinfo as j_common_ptr);    /* use common routine */
+}
+
+
+/*
+ * Abort processing of a JPEG compression operation,
+ * but don't destroy the object itself.
+ */
+
+pub unsafe fn jpeg_abort_compress(cinfo: j_compress_ptr) {
+    jpeg_abort(cinfo as j_common_ptr);    /* use common routine */
+}
+
+
+/*
+ * Forcibly suppress or un-suppress all quantization and Huffman tables.
+ * Marks all currently defined tables as already written (if suppress)
+ * or not written (if !suppress).  This will control whether they get emitted
+ * by a subsequent jpeg_start_compress call.
+ *
+ * This routine is exported for use by applications that want to produce
+ * abbreviated JPEG datastreams.  It logically belongs in jcparam.c, but
+ * since it is called by jpeg_start_compress, we put it here --- otherwise
+ * jcparam.o would be linked whether the application used it or not.
+ */
+
+pub unsafe fn jpeg_suppress_tables(cinfo: j_compress_ptr, suppress: boolean) {
+    let mut i: c_int;
+    let mut qtbl: *mut JQUANT_TBL;
+    let mut htbl: *mut JHUFF_TBL;
+
+    for i in 0..NUM_QUANT_TBLS as c_int {
+        if !(*cinfo).quant_tbl_ptrs[i as usize].is_null() {
+            qtbl = (*cinfo).quant_tbl_ptrs[i as usize];
+            (*qtbl).sent_table = suppress;
+        }
+    }
+
+    for i in 0..NUM_HUFF_TBLS as c_int {
+        if !(*cinfo).dc_huff_tbl_ptrs[i as usize].is_null() {
+            htbl = (*cinfo).dc_huff_tbl_ptrs[i as usize];
+            (*htbl).sent_table = suppress;
+        }
+        if !(*cinfo).ac_huff_tbl_ptrs[i as usize].is_null() {
+            htbl = (*cinfo).ac_huff_tbl_ptrs[i as usize];
+            (*htbl).sent_table = suppress;
+        }
+    }
+}
+
+
+/*
+ * Finish JPEG compression.
+ *
+ * If a multipass operating mode was selected, this may do a great deal of
+ * work including most of the actual output.
+ */
+
+pub unsafe fn jpeg_finish_compress(cinfo: j_compress_ptr) {
+    let mut iMCU_row: JDIMENSION;
+
+    if (*cinfo).global_state == CSTATE_SCANNING ||
+        (*cinfo).global_state == CSTATE_RAW_OK {
+        /* Terminate first pass */
+        if (*cinfo).next_scanline < (*cinfo).image_height {
+            // ERREXIT(cinfo, JERR_TOO_LITTLE_DATA);
+        }
+        if let Some(finish_pass_fn) = (*(*cinfo).master).finish_pass {
+            finish_pass_fn(cinfo as *mut c_void);
+        }
+    } else if (*cinfo).global_state != CSTATE_WRCOEFS {
+        // ERREXIT1(cinfo, JERR_BAD_STATE, cinfo->global_state);
+    }
+    /* Perform any remaining passes */
+    while (*(*cinfo).master).is_last_pass == FALSE {
+        if let Some(prepare_for_pass_fn) = (*(*cinfo).master).prepare_for_pass {
+            prepare_for_pass_fn(cinfo as *mut c_void);
+        }
+        iMCU_row = 0;
+        while iMCU_row < (*cinfo).total_iMCU_rows {
+            if !(*cinfo).progress.is_null() {
+                (*(*cinfo).progress).pass_counter = iMCU_row as core::ffi::c_long;
+                (*(*cinfo).progress).pass_limit = (*cinfo).total_iMCU_rows as core::ffi::c_long;
+                if let Some(progress_monitor_fn) = (*(*cinfo).progress).progress_monitor {
+                    progress_monitor_fn(cinfo as *mut c_void);
+                }
+            }
+            /* We bypass the main controller and invoke coef controller directly;
+             * all work is being done from the coefficient buffer.
+             */
+            if let Some(compress_data_fn) = (*(*cinfo).coef).compress_data {
+                if compress_data_fn(cinfo as *mut c_void, core::ptr::null_mut()) == FALSE {
+                    // ERREXIT(cinfo, JERR_CANT_SUSPEND);
+                }
+            }
+            iMCU_row += 1;
+        }
+        if let Some(finish_pass_fn) = (*(*cinfo).master).finish_pass {
+            finish_pass_fn(cinfo as *mut c_void);
+        }
+    }
+    /* Write EOI, do final cleanup */
+    if let Some(write_file_trailer_fn) = (*(*cinfo).marker).write_file_trailer {
+        write_file_trailer_fn(cinfo as *mut c_void);
+    }
+    if let Some(term_destination_fn) = (*(*cinfo).dest).term_destination {
+        term_destination_fn(cinfo as *mut c_void);
+    }
+    /* We can use jpeg_abort to release memory and reset global_state */
+    jpeg_abort(cinfo as j_common_ptr);
+}
+
+
+/*
+ * Write a special marker.
+ * This is only recommended for writing COM or APPn markers.
+ * Must be called after jpeg_start_compress() and before
+ * first call to jpeg_write_scanlines() or jpeg_write_raw_data().
+ */
+
+pub unsafe fn jpeg_write_marker(
+    cinfo: j_compress_ptr,
+    marker: c_int,
+    dataptr: *const JOCTET,
+    datalen: core::ffi::c_uint,
+) {
+    if (*cinfo).next_scanline != 0 ||
+        ((*cinfo).global_state != CSTATE_SCANNING &&
+         (*cinfo).global_state != CSTATE_RAW_OK &&
+         (*cinfo).global_state != CSTATE_WRCOEFS) {
+        // ERREXIT1(cinfo, JERR_BAD_STATE, cinfo->global_state);
+    }
+
+    if let Some(write_any_marker_fn) = (*(*cinfo).marker).write_any_marker {
+        write_any_marker_fn(cinfo as *mut c_void, marker, dataptr, datalen);
+    }
+}
+
+
+/*
+ * Alternate compression function: just write an abbreviated table file.
+ * Before calling this, all parameters and a data destination must be set up.
+ *
+ * To produce a pair of files containing abbreviated tables and abbreviated
+ * image data, one would proceed as follows:
+ *
+ *		initialize JPEG object
+ *		set JPEG parameters
+ *		set destination to table file
+ *		jpeg_write_tables(cinfo);
+ *		set destination to image file
+ *		jpeg_start_compress(cinfo, FALSE);
+ *		write data...
+ *		jpeg_finish_compress(cinfo);
+ *
+ * jpeg_write_tables has the side effect of marking all tables written
+ * (same as jpeg_suppress_tables(..., TRUE)).  Thus a subsequent start_compress
+ * will not re-emit the tables unless it is passed write_all_tables=TRUE.
+ */
+
+pub unsafe fn jpeg_write_tables(cinfo: j_compress_ptr) {
+    if (*cinfo).global_state != CSTATE_START {
+        // ERREXIT1(cinfo, JERR_BAD_STATE, cinfo->global_state);
+    }
+
+    /* (Re)initialize error mgr and destination modules */
+    if let Some(reset_error_mgr_fn) = (*(*cinfo).err).reset_error_mgr {
+        reset_error_mgr_fn(cinfo as *mut c_void);
+    }
+    if let Some(init_destination_fn) = (*(*cinfo).dest).init_destination {
+        init_destination_fn(cinfo as *mut c_void);
+    }
+    /* Initialize the marker writer ... bit of a crock to do it here. */
+    jinit_marker_writer(cinfo);
+    /* Write them tables! */
+    if let Some(write_tables_only_fn) = (*(*cinfo).marker).write_tables_only {
+        write_tables_only_fn(cinfo as *mut c_void);
+    }
+    /* And clean up. */
+    if let Some(term_destination_fn) = (*(*cinfo).dest).term_destination {
+        term_destination_fn(cinfo as *mut c_void);
+    }
+    /* We can use jpeg_abort to release memory. */
+    jpeg_abort(cinfo as j_common_ptr);
+}
