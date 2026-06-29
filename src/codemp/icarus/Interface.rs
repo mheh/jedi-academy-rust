05e9cd9058dabd275b3ee2cfac0e419f55a56208
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 20:54:10 -0500

    port oracle/codemp/icarus/Interface.cpp

---
+++ src/codemp/icarus/Interface.rs
@@ -0,0 +1,23 @@
+// Anything above this #include will be ignored by the compiler
+
+#![allow(non_snake_case)]
+
+use crate::codemp::icarus::interface_h::interface_export_t;
+
+// ICARUS Engine Interface File
+//
+// This file is the only section of the ICARUS systems that
+// is not directly portable from engine to engine.
+//
+// -- jweier
+
+/*
+fn Interface_Init(pe: *mut interface_export_t)
+{
+	// TODO: This is where you link up all your functions to the engine
+
+	// Example:
+	//
+	// (*pe).I_GetEntityByName = ENGINE_GetEntityByName;
+}
+*/
