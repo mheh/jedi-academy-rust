91274f91e2014eca11eaab845c3a5f393937a927
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-29 18:17:33 -0500

    port codemp/RMG/RM_Instance_Group.h

--- src/codemp/RMG/RM_Instance_Group_h.rs
+++ src/codemp/RMG/RM_Instance_Group_h.rs
@@ -1,236 +1,96 @@
-//! Mechanical port of `codemp/RMG/RM_Instance_Group.h`
-//!
-//! This is a faithful translation of the C++ header file RM_Instance_Group.h,
-//! preserving all original comments and structure.
-
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
+#![allow(dead_code)]
+
+// #pragma once
+// #if !defined(RM_INSTANCE_GROUP_H_INC)
+// #define RM_INSTANCE_GROUP_H_INC
+
+// #ifdef DEBUG_LINKING
+// 	#pragma message("...including RM_Instance_Group.h")
+// #endif
+
+use crate::codemp::RMG::GenericParser2_h::*;
+use crate::codemp::RMG::RM_InstanceFile_h::*;
+use crate::codemp::RMG::RM_Area_h::*;
+use crate::codemp::RMG::RM_Objective_h::*;
+use crate::codemp::qcommon::cm_randomterrain_h::*;
+use crate::codemp::RMG::RM_Instance_h::*;
 
 use core::ffi::{c_char, c_int, c_float};
 
-// DEBUG_LINKING pragma (preserved from original):
-// #ifdef DEBUG_LINKING
-//     #pragma message("...including RM_Instance_Group.h")
-// #endif
-
-// ============================================================================
-// AUTOMAP ENUM - Preserved from RM_Instance.h
-// ============================================================================
-// Original comment block from RM_Instance.h enum:
-pub const AUTOMAP_NONE: c_int = 0;
-pub const AUTOMAP_BLD: c_int = 1;
-pub const AUTOMAP_OBJ: c_int = 2;
-pub const AUTOMAP_START: c_int = 3;
-pub const AUTOMAP_END: c_int = 4;
-pub const AUTOMAP_ENEMY: c_int = 5;
-pub const AUTOMAP_FRIEND: c_int = 6;
-pub const AUTOMAP_WALL: c_int = 7;
-
-// ============================================================================
-// TYPE ALIASES
-// ============================================================================
-pub type qboolean = c_int;
-pub type vec_t = c_float;
-pub type vec3_t = [c_float; 3];
-pub type vec3pair_t = [[c_float; 3]; 2];
-
-// MAX_QPATH constant for fixed-size char arrays
-pub const MAX_QPATH: usize = 64;
-
-// ============================================================================
-// FORWARD DECLARATIONS for external types
-// ============================================================================
-/// Stub for unported `class CGPGroup` (GenericParser2.h).
-/// Holds configuration key-value pairs used during instance construction.
-pub struct CGPGroup {
-    _opaque: [u8; 0],
-}
-
-/// Stub for unported `class CRMInstanceFile` (RM_InstanceFile.h).
-/// Reference to an open instance file for creating sub-instances.
-pub struct CRMInstanceFile {
-    _opaque: [u8; 0],
-}
-
-/// Stub for unported `class CRMArea` (RM_Area.h).
-/// Represents an area in the map.
-pub struct CRMArea {
-    _opaque: [u8; 0],
-}
-
-/// Stub for unported `class CRMAreaManager` (RM_Area.h).
-/// Manages multiple areas in the map.
-pub struct CRMAreaManager {
-    _opaque: [u8; 0],
-}
-
-/// Stub for unported `class CRMObjective` (RM_Objective.h).
-/// Represents an objective in the mission.
-pub struct CRMObjective {
-    _opaque: [u8; 0],
-}
-
-/// Stub for unported `class CRandomTerrain` (RM_Terrain.h).
-/// Represents the random terrain generation system.
-pub struct CRandomTerrain {
-    _opaque: [u8; 0],
-}
-
-// ============================================================================
-// STL STUBS
-// ============================================================================
-/// Stub for C++ std::string.
-/// Represents a dynamic string type used internally by CRMInstance.
-#[repr(C)]
-pub struct std_string {
-    _private: [u8; 0],
-}
-
-/// Stub for C++ std::list<CRMInstance*>.
-/// Represents a doubly-linked list of instance pointers.
-#[repr(C)]
-pub struct rmInstanceList_t {
-    _private: [u8; 0],
-}
-
-// ============================================================================
-// CRMGroupInstance - Derived class (includes flattened base class fields)
-// ============================================================================
-// Preserved from RM_Instance_Group.h: class CRMGroupInstance : public CRMInstance
-//
-// CRMGroupInstance inherits from CRMInstance in the original C++ code.
-// In Rust with #[repr(C)], we flatten the base class fields directly to maintain
-// the exact memory layout expected by C++ code.
-//
-// Base class CRMInstance fields (from RM_Instance.h):
+// class CRMGroupInstance : public CRMInstance
 #[repr(C)]
 pub struct CRMGroupInstance {
-    // ---- CRMInstance base class fields (flattened for C++ layout compatibility) ----
-    // filter of entities inside of this
-    pub mFilter: [c_char; MAX_QPATH],
-    // team specific filter
-    pub mTeamFilter: [c_char; MAX_QPATH],
+    // protected:
 
-    // Bounding box for instance itself
-    pub mBounds: vec3pair_t,
-
-    // Position of the instance
-    pub mArea: *mut CRMArea,
-
-    // Objective associated with this instance
-    pub mObjective: *mut CRMObjective,
-
-    // optional instance specific strings for objective
-    // message outputed when objective is completed
-    pub mMessage: std_string,
-    // description of objective
-    pub mDescription: std_string,
-    // more info for objective
-    pub mInfo: std_string,
-
-    // Radius to space instances with
-    pub mSpacingRadius: c_float,
-    // Radius to flatten under instances
-    pub mFlattenRadius: c_float,
-
-    // Line of spacing radius's, forces locket
-    pub mSpacingLine: c_int,
-    // Origin cant move
-    pub mLockOrigin: bool,
-
-    // allow surface sprites under instance?
-    pub mSurfaceSprites: bool,
-
-    // show which symbol on automap 0=none
-    pub mAutomapSymbol: c_int,
-
-    // id of entity spawned
-    pub mEntityID: c_int,
-    // blue or red side
-    pub mSide: c_int,
-    // mirror origin, angle
-    pub mMirror: c_int,
-
-    // height to flatten land
-    pub mFlattenHeight: c_int,
-
-    // ---- CRMGroupInstance-specific fields (protected in original C++) ----
     pub mInstances: rmInstanceList_t,
     pub mConfineRadius: c_float,
     pub mPaddingSize: c_float,
 }
 
 impl CRMGroupInstance {
-    // ========================================================================
-    // Public methods (from original C++ class)
-    // ========================================================================
+    // public:
 
-    /// CRMGroupInstance( CGPGroup* instGroup, CRMInstanceFile& instFile);
-    pub fn new(instGroup: *mut CGPGroup, instFile: &mut CRMInstanceFile) -> Self {
+    // CRMGroupInstance( CGPGroup* instGroup, CRMInstanceFile& instFile);
+    pub unsafe fn new(instGroup: *mut CGPGroup, instFile: &mut CRMInstanceFile) -> Self {
         unimplemented!()
     }
 
-    /// ~CRMGroupInstance();
-    pub fn drop(&mut self) {
-        // Destructor
-    }
+    // ~CRMGroupInstance();
 
-    /// virtual bool PreSpawn ( CRandomTerrain* terrain, qboolean IsServer );
-    pub fn PreSpawn(&mut self, terrain: *mut CRandomTerrain, IsServer: qboolean) -> bool {
+    // virtual bool		PreSpawn			( CRandomTerrain* terrain, qboolean IsServer );
+    pub unsafe fn PreSpawn(&mut self, terrain: *mut CRandomTerrain, IsServer: qboolean) -> bool {
         unimplemented!()
     }
 
-    /// virtual bool Spawn ( CRandomTerrain* terrain, qboolean IsServer );
-    pub fn Spawn(&mut self, terrain: *mut CRandomTerrain, IsServer: qboolean) -> bool {
+    // virtual bool		Spawn				( CRandomTerrain* terrain, qboolean IsServer );
+    pub unsafe fn Spawn(&mut self, terrain: *mut CRandomTerrain, IsServer: qboolean) -> bool {
         unimplemented!()
     }
 
-    /// virtual void Preview ( const vec3_t from );
-    pub fn Preview(&self, from: *const vec3_t) {
+    // virtual void		Preview				( const vec3_t from );
+    pub unsafe fn Preview(&self, from: vec3_t) {
         unimplemented!()
     }
 
-    /// virtual void SetFilter ( const char *filter );
-    pub fn SetFilter(&mut self, filter: *const c_char) {
+    // virtual void		SetFilter			( const char *filter );
+    pub unsafe fn SetFilter(&mut self, filter: *const c_char) {
         unimplemented!()
     }
 
-    /// virtual void SetTeamFilter ( const char *teamFilter );
-    pub fn SetTeamFilter(&mut self, teamFilter: *const c_char) {
+    // virtual void		SetTeamFilter		( const char *teamFilter );
+    pub unsafe fn SetTeamFilter(&mut self, teamFilter: *const c_char) {
         unimplemented!()
     }
 
-    /// virtual void SetArea ( CRMAreaManager* amanager, CRMArea* area );
-    pub fn SetArea(&mut self, amanager: *mut CRMAreaManager, area: *mut CRMArea) {
+    // virtual void		SetArea				( CRMAreaManager* amanager, CRMArea* area );
+    pub unsafe fn SetArea(&mut self, amanager: *mut CRMAreaManager, area: *mut CRMArea) {
         unimplemented!()
     }
 
-    /// virtual int GetPreviewColor ( )		{ return (255<<24)+(255<<8); }
+    // virtual int			GetPreviewColor		( )		{ return (255<<24)+(255<<8); }
     pub fn GetPreviewColor(&self) -> c_int {
-        (255 << 24) + (255 << 8)
+        (255_i32).wrapping_shl(24).wrapping_add((255_i32).wrapping_shl(8))
     }
 
-    /// virtual float GetSpacingRadius ( )		{ return 0; }
-    pub fn GetSpacingRadius(&self) -> c_float {
-        0.0
-    }
+    // virtual float		GetSpacingRadius	( )		{ return 0; }
+    pub fn GetSpacingRadius(&self) -> c_float { 0.0 }
 
-    /// virtual float GetFlattenRadius ( )		{ return 0; }
-    pub fn GetFlattenRadius(&self) -> c_float {
-        0.0
-    }
+    // virtual float		GetFlattenRadius	( )		{ return 0; }
+    pub fn GetFlattenRadius(&self) -> c_float { 0.0 }
 
-    /// virtual void SetMirror(int mirror);
-    pub fn SetMirror(&mut self, mirror: c_int) {
+    // virtual void  		SetMirror(int mirror);
+    pub unsafe fn SetMirror(&mut self, mirror: c_int) {
         unimplemented!()
     }
 
-    // ========================================================================
-    // Protected methods (from original C++ class)
-    // ========================================================================
+    // protected:
 
-    /// void RemoveInstances ( );
-    pub fn RemoveInstances(&mut self) {
+    // void	RemoveInstances	 ( );
+    unsafe fn RemoveInstances(&mut self) {
         unimplemented!()
     }
 }
+
+// #endif
