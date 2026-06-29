08d0e5eeca00827ab91a03591a89e00279ab6ab5
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-29 17:43:22 -0500

    port code/RMG/RM_Objective.cpp

--- src/code/RMG/RM_Objective.rs
+++ src/code/RMG/RM_Objective.rs
@@ -1,4 +1,4 @@
-/************************************************************************************************
+/* ***************************************************************************************
  *
  * RM_Objective.cpp
  *
@@ -10,62 +10,19 @@
 #![allow(non_snake_case)]
 
 use core::ffi::{c_int, c_char};
-use std::ptr;
 
-// LOCAL STUB: Forward declarations for types used in this file
-pub struct CGPGroup {
-    _opaque: [u8; 0],
-}
+// #include "../server/exe_headers.h"
+use crate::code::server::exe_headers_h::*;
+// #include "rm_headers.h"  — brings in CRMObjective (via RM_Objective.h) and CGPGroup (via genericparser2.h)
+use crate::code::RMG::rm_headers_h::*;
 
-// Forward declaration: C++ std::string (opaque - exact layout is implementation-dependent)
-// We represent it as a zero-sized type since we cannot directly use it across FFI
-pub struct string {
-    _opaque: [u8; 0],
-}
-
-// porting stub: external functions
+// atoi is a C stdlib function (not a module); declare it directly
 extern "C" {
-    /// atoi - Convert C string to integer
-    /// int atoi(const char *nptr);
     fn atoi(nptr: *const c_char) -> c_int;
-
-    /// va - Variable argument string formatting (like sprintf)
-    /// const char* va(const char *fmt, ...);
-    fn va(format: *const c_char, ...) -> *const c_char;
-}
-
-impl CGPGroup {
-    /// Stub for CGPGroup::FindPairValue
-    /// const char* FindPairValue(const char* key, const char* default);
-    fn FindPairValue(&self, key: *const c_char, default_val: *const c_char) -> *const c_char {
-        // This is an opaque method - implementation is in C++
-        ptr::null()
-    }
-
-    /// Stub for CGPGroup::GetName
-    /// const char* GetName(void);
-    fn GetName(&self) -> *const c_char {
-        // This is an opaque method - implementation is in C++
-        ptr::null()
-    }
-}
-
-#[repr(C)]
-pub struct CRMObjective {
-    pub mCompleted: bool,
-    pub mActive: bool,
-    pub mPriority: c_int,
-    pub mOrderIndex: c_int,
-    pub mCompleteSoundID: c_int,
-    pub mMessage: string,
-    pub mDescription: string,
-    pub mInfo: string,
-    pub mName: string,
-    pub mTrigger: string,
 }
 
 impl CRMObjective {
-    /************************************************************************************************
+    /* ***************************************************************************************
      * CRMObjective::CRMObjective
      *	Constructs a random mission objective and fills in the default properties
      *
@@ -76,91 +33,51 @@ impl CRMObjective {
      *	none
      *
      ************************************************************************************************/
-    pub fn new(group: *mut CGPGroup) -> Self {
-        unsafe {
-            let mut obj: CRMObjective = std::mem::zeroed();
+    pub unsafe fn new(group: *mut CGPGroup) -> Self {
+        let mut obj: Self = core::mem::zeroed();
+        obj.SetPriority(atoi((*group).FindPairValue(
+            b"priority\0".as_ptr() as *const c_char,
+            b"0\0".as_ptr() as *const c_char,
+        )));
+        // Capture priority once to avoid overlapping borrows in the va() argument expressions
+        let prio = obj.GetPriority();
+        obj.SetMessage((*group).FindPairValue(
+            b"message\0".as_ptr() as *const c_char,
+            va(b"Objective %i Completed\0".as_ptr() as *const c_char, prio),
+        ));
+        obj.SetDescription((*group).FindPairValue(
+            b"description\0".as_ptr() as *const c_char,
+            va(b"Objective %i\0".as_ptr() as *const c_char, prio),
+        ));
+        obj.SetInfo((*group).FindPairValue(
+            b"info\0".as_ptr() as *const c_char,
+            va(b"Info %i\0".as_ptr() as *const c_char, prio),
+        ));
+        obj.SetTrigger((*group).FindPairValue(
+            b"trigger\0".as_ptr() as *const c_char,
+            b"\0".as_ptr() as *const c_char,
+        ));
+        obj.SetName((*group).GetName());
 
-            // SetPriority(atoi(group->FindPairValue("priority", "0")));
-            if !group.is_null() {
-                let priority_str = (*group).FindPairValue(
-                    b"priority\0".as_ptr() as *const c_char,
-                    b"0\0".as_ptr() as *const c_char,
-                );
-                obj.mPriority = atoi(priority_str);
+    /*	const char * soundPath = group->FindPairValue("completed_sound", "" );
+        if (soundPath)
+            mCompleteSoundID = G_SoundIndex(soundPath);
+    */
 
-                // SetMessage( group->FindPairValue("message",va("Objective %i Completed", GetPriority()) ) );
-                let message_default = va(
-                    b"Objective %i Completed\0".as_ptr() as *const c_char,
-                    obj.mPriority,
-                );
-                let _message_str = (*group).FindPairValue(
-                    b"message\0".as_ptr() as *const c_char,
-                    message_default,
-                );
-                // Note: mMessage is an opaque C++ string, so assignment happens through C++
+        obj.mCompleted  = false;
+        obj.mOrderIndex = -1;
 
-                // SetDescription(group->FindPairValue("description",va("Objective %i", GetPriority()) ) );
-                let description_default = va(
-                    b"Objective %i\0".as_ptr() as *const c_char,
-                    obj.mPriority,
-                );
-                let _description_str = (*group).FindPairValue(
-                    b"description\0".as_ptr() as *const c_char,
-                    description_default,
-                );
-                // Note: mDescription is an opaque C++ string, so assignment happens through C++
-
-                // SetInfo(group->FindPairValue("info",va("Info %i", GetPriority()) ) );
-                let info_default = va(
-                    b"Info %i\0".as_ptr() as *const c_char,
-                    obj.mPriority,
-                );
-                let _info_str = (*group).FindPairValue(
-                    b"info\0".as_ptr() as *const c_char,
-                    info_default,
-                );
-                // Note: mInfo is an opaque C++ string, so assignment happens through C++
-
-                // SetTrigger(group->FindPairValue("trigger",""));
-                let _trigger_str = (*group).FindPairValue(
-                    b"trigger\0".as_ptr() as *const c_char,
-                    b"\0".as_ptr() as *const c_char,
-                );
-                // Note: mTrigger is an opaque C++ string, so assignment happens through C++
-
-                // SetName(group->GetName());
-                let _name_str = (*group).GetName();
-                // Note: mName is an opaque C++ string, so assignment happens through C++
-            } else {
-                obj.mPriority = 0;
-            }
-
-            // mCompleted  = false;
-            obj.mCompleted = false;
-
-            // mOrderIndex = -1;
-            obj.mOrderIndex = -1;
-
-            // If no priority was specified for this objective then its active by default.
-            // if ( GetPriority ( ) )
-            // {
-            //     mActive	= false;
-            // }
-            // else
-            // {
-            //     mActive = true;
-            // }
-            if obj.mPriority != 0 {
-                obj.mActive = false;
-            } else {
-                obj.mActive = true;
-            }
-
-            obj
+        // If no priority was specified for this objective then its active by default.
+        if obj.GetPriority() != 0 {
+            obj.mActive = false;
+        } else {
+            obj.mActive = true;
         }
+
+        obj
     }
 
-    /************************************************************************************************
+    /* ***************************************************************************************
      * CRMObjective::FindRandomTrigger
      *	Searches the entitySystem form a random arioche trigger that matches the objective name
      *
@@ -171,53 +88,52 @@ impl CRMObjective {
      *	trigger: a random trigger or NULL if one couldnt be found
      *
      ************************************************************************************************/
-    // Implementation commented out in original C++ code - preserved as-is
-    // /*CTriggerAriocheObjective* CRMObjective::FindRandomTrigger ( )
-    // {
-    //     CEntity*	search;
-    //     CEntity*	triggers[20];
-    //     int			numTriggers;
-    //
-    //     // Start off the first trigger
-    //     numTriggers = 0;
-    //     search      = entitySystem->GetEntityFromClassname ( NULL, "trigger_arioche_objective" );
-    //
-    //     // Make a list of triggers
-    //     while ( numTriggers < 20 && search )
-    //     {
-    //         CTriggerAriocheObjective* trigger = (CTriggerAriocheObjective*) search;
-    //
-    //         // Move on to the next trigger
-    //         search = entitySystem->GetEntityFromClassname ( search, "trigger_arioche_objective" );
-    //
-    //         // See if this trigger is already in use
-    //         if ( trigger->GetObjective ( ) )
-    //         {
-    //             continue;
-    //         }
-    //
-    //         // If the objective names dont match then ignore this trigger
-    //         if ( stricmp ( trigger->GetObjectiveName ( ), GetTrigger() ) )
-    //         {
-    //             continue;
-    //         }
-    //
-    //         // Add the trigger to the list
-    //         triggers[numTriggers++] = trigger;
-    //     }
-    //
-    //     // If no matching triggers then just return NULL
-    //     if ( 0 == numTriggers )
-    //     {
-    //         return NULL;
-    //     }
-    //
-    //     // Return a random choice from the trigger list
-    //     return (CTriggerAriocheObjective*)triggers[TheRandomMissionManager->GetLandScape()->irand(0,numTriggers-1)];
-    // }
-    // */
+    /*CTriggerAriocheObjective* CRMObjective::FindRandomTrigger ( )
+    {
+        CEntity*	search;
+        CEntity*	triggers[20];
+        int			numTriggers;
 
-    /************************************************************************************************
+        // Start off the first trigger
+        numTriggers = 0;
+        search      = entitySystem->GetEntityFromClassname ( NULL, "trigger_arioche_objective" );
+
+        // Make a list of triggers
+        while ( numTriggers < 20 && search )
+        {
+            CTriggerAriocheObjective* trigger = (CTriggerAriocheObjective*) search;
+
+            // Move on to the next trigger
+            search = entitySystem->GetEntityFromClassname ( search, "trigger_arioche_objective" );
+
+            // See if this trigger is already in use
+            if ( trigger->GetObjective ( ) )
+            {
+                continue;
+            }
+
+            // If the objective names dont match then ignore this trigger
+            if ( stricmp ( trigger->GetObjectiveName ( ), GetTrigger() ) )
+            {
+                continue;
+            }
+
+            // Add the trigger to the list
+            triggers[numTriggers++] = trigger;
+        }
+
+        // If no matching triggers then just return NULL
+        if ( 0 == numTriggers )
+        {
+            return NULL;
+        }
+
+        // Return a random choice from the trigger list
+        return (CTriggerAriocheObjective*)triggers[TheRandomMissionManager->GetLandScape()->irand(0,numTriggers-1)];
+    }
+    */
+
+    /* ***************************************************************************************
      * CRMObjective::Link
      *	Links the objective into the world using the current state of the world to determine
      *  where it should link
@@ -230,16 +146,16 @@ impl CRMObjective {
      *  false: objective failed to link
      *
      ************************************************************************************************/
-    pub fn Link(&mut self) -> bool {
-        // /*	CTriggerAriocheObjective* trigger;
-        //
-        // 	// Look for a random trigger to associate this objective to.
-        // 	trigger = FindRandomTrigger ( );
-        // 	if ( NULL != trigger )
-        // 	{
-        // 		trigger->SetObjective ( this );
-        // 	}
-        // */
+    pub unsafe fn Link(&mut self) -> bool {
+    /*	CTriggerAriocheObjective* trigger;
+
+        // Look for a random trigger to associate this objective to.
+        trigger = FindRandomTrigger ( );
+        if ( NULL != trigger )
+        {
+            trigger->SetObjective ( this );
+        }
+    */
         true
     }
 }
