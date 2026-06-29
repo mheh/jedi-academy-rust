8988ad1ae195a885273c6fde3aeda25fa8738560
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 20:54:09 -0500

    port oracle/code/cgame/FxParsing.cpp

---
+++ src/code/cgame/FxParsing.rs
@@ -0,0 +1,8 @@
+//! Mechanical port of `code/cgame/FxParsing.cpp`.
+
+// this include must remain at the top of every FXxxxx.CPP file
+
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(dead_code)]
