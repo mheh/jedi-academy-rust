b67653d5a8f21ce36ebeb5ee4f40d8484850773f
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 20:54:09 -0500

    port oracle/codemp/ui/ui_util.c

---
+++ src/codemp/ui/ui_util.rs
@@ -0,0 +1,11 @@
+// ui_util.c
+//
+// origin: rad
+// new ui support stuff
+//
+// memory, string alloc
+
+#[allow(non_snake_case)]
+pub fn RichIsGettingTiredOfEmptyFileVMCompilerWarnings() {
+    let andUnderstandablySo = 0i32;
+}
