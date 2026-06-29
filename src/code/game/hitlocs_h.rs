ff789b77ed05c2724c1f25e30b51f70c290991bf
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 20:54:11 -0500

    port oracle/code/game/hitlocs.h

---
+++ src/code/game/hitlocs_h.rs
@@ -0,0 +1,37 @@
+// Opaque type defined elsewhere in the C translation
+#[repr(C)]
+pub struct stringID_table_t;
+
+//# hitloc_e
+#[repr(C)]
+pub enum hitloc_e {
+    HL_NONE = 0,
+    HL_FOOT_RT,
+    HL_FOOT_LT,
+    HL_LEG_RT,
+    HL_LEG_LT,
+    HL_WAIST,
+    HL_BACK_RT,
+    HL_BACK_LT,
+    HL_BACK,
+    HL_CHEST_RT,
+    HL_CHEST_LT,
+    HL_CHEST,
+    HL_ARM_RT,
+    HL_ARM_LT,
+    HL_HAND_RT,
+    HL_HAND_LT,
+    HL_HEAD,
+    HL_GENERIC1,
+    HL_GENERIC2,
+    HL_GENERIC3,
+    HL_GENERIC4,
+    HL_GENERIC5,
+    HL_GENERIC6,
+    //# #eol
+    HL_MAX,
+}
+
+extern "C" {
+    pub static HLTable: [stringID_table_t; 0];
+}
