be753798c7528118a2459cbb2135ceb47d5db3b2
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 20:54:13 -0500

    port oracle/code/jpeg-6/jinclude.h

---
+++ src/code/jpeg-6/jinclude_h.rs
@@ -0,0 +1,123 @@
+/*
+ * jinclude.h
+ *
+ * Copyright (C) 1991-1994, Thomas G. Lane.
+ * This file is part of the Independent JPEG Group's software.
+ * For conditions of distribution and use, see the accompanying README file.
+ *
+ * This file exists to provide a single place to fix any problems with
+ * including the wrong system include files.  (Common problems are taken
+ * care of by the standard jconfig symbols, but on really weird systems
+ * you may have to edit this file.)
+ *
+ * NOTE: this file is NOT intended to be included by applications using the
+ * JPEG library.  Most applications need only include jpeglib.h.
+ */
+
+// NOTE: Windows pragma warnings (MSVC _WIN32 directives) from original:
+// #pragma warning(disable : 4018)     // signed/unsigned mismatch
+// #pragma warning(disable : 4032)
+// #pragma warning(disable : 4051)
+// #pragma warning(disable : 4057)		// slightly different base types
+// #pragma warning(disable : 4100)		// unreferenced formal parameter
+// #pragma warning(disable : 4115)
+// #pragma warning(disable : 4125)		// decimal digit terminates octal escape sequence
+// #pragma warning(disable : 4127)		// conditional expression is constant
+// #pragma warning(disable : 4136)
+// #pragma warning(disable : 4152)		// nonstandard extension, function/data pointer conversion in expression
+// #pragma warning(disable : 4201)
+// #pragma warning(disable : 4214)
+// #pragma warning(disable : 4244)
+// #pragma warning(disable : 4305)		// truncation from const double to float
+// #pragma warning(disable : 4310)		// cast truncates constant value
+// #pragma warning(disable:  4505) 	// unreferenced local function has been removed
+// #pragma warning(disable : 4514)
+// #pragma warning(disable : 4702)		// unreachable code
+// #pragma warning(disable : 4711)		// selected for automatic inline expansion
+// #pragma warning(disable : 4220)		// varargs matches remaining parameters
+// #pragma warning(disable : 4761)		// integral size mismatch
+
+/* Include auto-config file to find out which system include files we need. */
+
+// NOTE: In C this includes "../jpeg-6/jconfig.h"
+// #define JCONFIG_INCLUDED	/* so that jpeglib.h doesn't do it again */
+
+/*
+ * We need the NULL macro and size_t typedef.
+ * On an ANSI-conforming system it is sufficient to include <stddef.h>.
+ * Otherwise, we get them from <stdlib.h> or <stdio.h>; we may have to
+ * pull in <sys/types.h> as well.
+ * Note that the core JPEG library does not require <stdio.h>;
+ * only the default error handler and data source/destination modules do.
+ * But we must pull it in because of the references to FILE in jpeglib.h.
+ * You can remove those references if you want to compile without <stdio.h>.
+ */
+
+use core::ffi::c_void;
+
+/*
+ * We need memory copying and zeroing functions, plus strncpy().
+ * ANSI and System V implementations declare these in <string.h>.
+ * BSD doesn't have the mem() functions, but it does have bcopy()/bzero().
+ * Some systems may declare memset and memcpy in <memory.h>.
+ *
+ * NOTE: we assume the size parameters to these functions are of type size_t.
+ * Change the casts in these macros if not!
+ */
+
+extern "C" {
+    fn memset(s: *mut c_void, c: i32, n: usize) -> *mut c_void;
+    fn memcpy(dest: *mut c_void, src: *const c_void, n: usize) -> *mut c_void;
+}
+
+// MEMZERO(target,size)	memset((void *)(target), 0, (size_t)(size))
+#[inline]
+pub unsafe fn MEMZERO(target: *mut c_void, size: usize) {
+    memset(target, 0, size);
+}
+
+// MEMCOPY(dest,src,size)	memcpy((void *)(dest), (const void *)(src), (size_t)(size))
+#[inline]
+pub unsafe fn MEMCOPY(dest: *mut c_void, src: *const c_void, size: usize) {
+    memcpy(dest, src, size);
+}
+
+/*
+ * In ANSI C, and indeed any rational implementation, size_t is also the
+ * type returned by sizeof().  However, it seems there are some irrational
+ * implementations out there, in which sizeof() returns an int even though
+ * size_t is defined as long or unsigned long.  To ensure consistent results
+ * we always use this SIZEOF() macro in place of using sizeof() directly.
+ */
+
+// SIZEOF(object)	((size_t) sizeof(object))
+// Note: In Rust, we use a generic const function that returns usize (equivalent to size_t)
+#[inline]
+pub const fn SIZEOF<T>() -> usize {
+    core::mem::size_of::<T>()
+}
+
+/*
+ * The modules that use fread() and fwrite() always invoke them through
+ * these macros.  On some systems you may need to twiddle the argument casts.
+ * CAUTION: argument order is different from underlying functions!
+ */
+
+extern "C" {
+    fn fread(ptr: *mut c_void, size: usize, nmemb: usize, stream: *mut c_void) -> usize;
+    fn fwrite(ptr: *const c_void, size: usize, nmemb: usize, stream: *mut c_void) -> usize;
+}
+
+// JFREAD(file,buf,sizeofbuf)
+// ((size_t) fread((void *) (buf), (size_t) 1, (size_t) (sizeofbuf), (file)))
+#[inline]
+pub unsafe fn JFREAD(file: *mut c_void, buf: *mut c_void, sizeofbuf: usize) -> usize {
+    fread(buf, 1, sizeofbuf, file) as usize
+}
+
+// JFWRITE(file,buf,sizeofbuf)
+// ((size_t) fwrite((const void *) (buf), (size_t) 1, (size_t) (sizeofbuf), (file)))
+#[inline]
+pub unsafe fn JFWRITE(file: *mut c_void, buf: *const c_void, sizeofbuf: usize) -> usize {
+    fwrite(buf, 1, sizeofbuf, file) as usize
+}
