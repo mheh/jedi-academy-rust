2d40bd4ae75d3568383ecaa48ce0403d679cf5ba
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 20:54:12 -0500

    port oracle/code/cgame/strip_objectives.h

---
+++ src/code/cgame/strip_objectives_h.rs
@@ -0,0 +1,72 @@
+use core::ffi::c_int;
+
+/********************************************************************************
+
+the objectives... duh...
+
+********************************************************************************/
+
+pub const PACKAGE_OBJECTIVES: c_int = 0x08;
+
+pub const OBJECTIVES_KEJIM_POST_OBJ1: c_int = 0x0800;
+pub const OBJECTIVES_KEJIM_POST_OBJ2: c_int = 0x0801;
+pub const OBJECTIVES_KEJIM_BASE_OBJ1: c_int = 0x0802;
+pub const OBJECTIVES_KEJIM_BASE_OBJ2: c_int = 0x0803;
+pub const OBJECTIVES_KEJIM_BASE_OBJ3: c_int = 0x0804;
+pub const OBJECTIVES_ARTUS_MINE_OBJ1: c_int = 0x0805;
+pub const OBJECTIVES_ARTUS_MINE_OBJ2: c_int = 0x0806;
+pub const OBJECTIVES_ARTUS_MINE_OBJ3: c_int = 0x0807;
+pub const OBJECTIVES_ARTUS_DETENTION_OBJ1: c_int = 0x0808;
+pub const OBJECTIVES_ARTUS_DETENTION_OBJ2: c_int = 0x0809;
+pub const OBJECTIVES_ARTUS_TOPSIDE_OBJ1: c_int = 0x080a;
+pub const OBJECTIVES_ARTUS_TOPSIDE_OBJ2: c_int = 0x080b;
+pub const OBJECTIVES_YAVIN_TEMPLE_OBJ1: c_int = 0x080c;
+pub const OBJECTIVES_YAVIN_TRIAL_OBJ1: c_int = 0x080d;
+pub const OBJECTIVES_YAVIN_TRIAL_OBJ2: c_int = 0x080e;
+pub const OBJECTIVES_NS_STREETS_OBJ1: c_int = 0x080f;
+pub const OBJECTIVES_NS_STREETS_OBJ2: c_int = 0x0810;
+pub const OBJECTIVES_NS_STREETS_OBJ3: c_int = 0x0811;
+pub const OBJECTIVES_NS_HIDEOUT_OBJ1: c_int = 0x0812;
+pub const OBJECTIVES_NS_HIDEOUT_OBJ2: c_int = 0x0813;
+pub const OBJECTIVES_NS_STARPAD_OBJ1: c_int = 0x0814;
+pub const OBJECTIVES_NS_STARPAD_OBJ2: c_int = 0x0815;
+pub const OBJECTIVES_NS_STARPAD_OBJ3: c_int = 0x0816;
+pub const OBJECTIVES_NS_STARPAD_OBJ4: c_int = 0x0817;
+pub const OBJECTIVES_NS_STARPAD_OBJ5: c_int = 0x0818;
+pub const OBJECTIVES_BESPIN_UNDERCITY_OBJ1: c_int = 0x0819;
+pub const OBJECTIVES_BESPIN_UNDERCITY_OBJ2: c_int = 0x081a;
+pub const OBJECTIVES_BESPIN_STREETS_OBJ1: c_int = 0x081b;
+pub const OBJECTIVES_BESPIN_STREETS_OBJ2: c_int = 0x081c;
+pub const OBJECTIVES_BESPIN_PLATFORM_OBJ1: c_int = 0x081d;
+pub const OBJECTIVES_BESPIN_PLATFORM_OBJ2: c_int = 0x081e;
+pub const OBJECTIVES_CAIRN_BAY_OBJ1: c_int = 0x081f;
+pub const OBJECTIVES_CAIRN_BAY_OBJ2: c_int = 0x0820;
+pub const OBJECTIVES_CAIRN_ASSEMBLY_OBJ1: c_int = 0x0821;
+pub const OBJECTIVES_CAIRN_REACTOR_OBJ1: c_int = 0x0822;
+pub const OBJECTIVES_CAIRN_REACTOR_OBJ2: c_int = 0x0823;
+pub const OBJECTIVES_CAIRN_DOCK1_OBJ1: c_int = 0x0824;
+pub const OBJECTIVES_CAIRN_DOCK1_OBJ2: c_int = 0x0825;
+pub const OBJECTIVES_DOOM_COMM_OBJ1: c_int = 0x0826;
+pub const OBJECTIVES_DOOM_COMM_OBJ2: c_int = 0x0827;
+pub const OBJECTIVES_DOOM_COMM_OBJ3: c_int = 0x0828;
+pub const OBJECTIVES_DOOM_DETENTION_OBJ1: c_int = 0x0829;
+pub const OBJECTIVES_DOOM_DETENTION_OBJ2: c_int = 0x082a;
+pub const OBJECTIVES_DOOM_SHIELDS_OBJ1: c_int = 0x082b;
+pub const OBJECTIVES_DOOM_SHIELDS_OBJ2: c_int = 0x082c;
+pub const OBJECTIVES_YAVIN_SWAMP_OBJ1: c_int = 0x082d;
+pub const OBJECTIVES_YAVIN_SWAMP_OBJ2: c_int = 0x082e;
+pub const OBJECTIVES_YAVIN_CANYON_OBJ1: c_int = 0x082f;
+pub const OBJECTIVES_YAVIN_CANYON_OBJ2: c_int = 0x0830;
+pub const OBJECTIVES_YAVIN_COURTYARD_OBJ1: c_int = 0x0831;
+pub const OBJECTIVES_YAVIN_COURTYARD_OBJ2: c_int = 0x0832;
+pub const OBJECTIVES_YAVIN_FINAL_OBJ1: c_int = 0x0833;
+pub const OBJECTIVES_KEJIM_POST_OBJ3: c_int = 0x0834;
+pub const OBJECTIVES_KEJIM_POST_OBJ4: c_int = 0x0835;
+pub const OBJECTIVES_KEJIM_POST_OBJ5: c_int = 0x0836;
+pub const OBJECTIVES_ARTUS_DETENTION_OBJ3: c_int = 0x0837;
+pub const OBJECTIVES_DOOM_COMM_OBJ4: c_int = 0x0838;
+pub const OBJECTIVES_DOOM_SHIELDS_OBJ3: c_int = 0x0839;
+pub const OBJECTIVES_DEMO_OBJ1: c_int = 0x083a;
+pub const OBJECTIVES_DEMO_OBJ2: c_int = 0x083b;
+pub const OBJECTIVES_DEMO_OBJ3: c_int = 0x083c;
+pub const OBJECTIVES_DEMO_OBJ4: c_int = 0x083d;
