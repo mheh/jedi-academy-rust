99ed5dcacf57d273c480cf62d82f265060615bf7
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 20:54:09 -0500

    port oracle/code/game/npc_headers.h

---
+++ src/code/game/npc_headers_h.rs
@@ -0,0 +1,8 @@
+// PCH header file organiser for  NPC_xxxx cpp files' most commonly used headers
+//
+// Original includes (organized by this header):
+// - b_local.h
+// - anims.h
+// - g_functions.h
+// - g_nav.h
+// - g_navigator.h
