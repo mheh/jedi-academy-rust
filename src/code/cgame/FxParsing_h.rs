8d1e929fa27186f1d1e9381876cb2abbe1130228
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-28 20:54:09 -0500

    port oracle/code/cgame/FxParsing.h

---
+++ src/code/cgame/FxParsing_h.rs
@@ -0,0 +1,6 @@
+// FxParsing.h
+// Original C header guards (preprocessor boilerplate preserved as comments):
+// #pragma once
+// #if !defined(FX_PARSING_H_INC)
+// #define FX_PARSING_H_INC
+// #endif // FX_PARSING_H
