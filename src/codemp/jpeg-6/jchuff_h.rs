6e9a46143c1787c3a28623ffcabe59b71ea796da
Milo Hehmsoth <mhehmsoth@gmail.com>
2026-06-29 21:32:18 -0500

    port codemp/jpeg-6/jchuff.h

--- src/codemp/jpeg-6/jchuff_h.rs
+++ src/codemp/jpeg-6/jchuff_h.rs
@@ -10,47 +10,35 @@
  * progressive encoder (jcphuff.c).  No other modules need to see these.
  */
 
-use core::ffi::{c_char, c_uint, c_long};
+/* Trusted import — JHUFF_TBL, jpeg_compress_struct, j_compress_ptr are defined in jpeglib_h */
+use crate::codemp::jpeg_6::jpeglib_h::*;
+
+#[allow(unused_imports)]
+use core::ffi::{c_char, c_long, c_uint};
 
 /* Derived data constructed for each Huffman table */
 
+#[allow(non_camel_case_types)]
 #[repr(C)]
 pub struct c_derived_tbl {
-    pub ehufco: [c_uint; 256],  // code for each symbol
-    pub ehufsi: [c_char; 256],  // length of code for each symbol
-    // If no code has been allocated for a symbol S, ehufsi[S] contains 0
+    pub ehufco: [c_uint; 256],  /* code for each symbol */
+    pub ehufsi: [c_char; 256],  /* length of code for each symbol */
+    /* If no code has been allocated for a symbol S, ehufsi[S] contains 0 */
 }
 
 /* Short forms of external names for systems with brain-damaged linkers. */
 
-// In C: #ifdef NEED_SHORT_EXTERNAL_NAMES
-// #define jpeg_make_c_derived_tbl	jMkCDerived
-// #define jpeg_gen_optimal_table	jGenOptTbl
-// #endif
+// #ifdef NEED_SHORT_EXTERNAL_NAMES
+// #define jpeg_make_c_derived_tbl  jMkCDerived
+// #define jpeg_gen_optimal_table   jGenOptTbl
+// #endif /* NEED_SHORT_EXTERNAL_NAMES */
 
-/* Expand a Huffman table definition into the derived format */
 extern "C" {
-    pub fn jpeg_make_c_derived_tbl(
-        cinfo: *mut j_compress_struct,
-        htbl: *mut JHUFF_TBL,
-        pdtbl: *mut *mut c_derived_tbl,
-    );
+    /* Expand a Huffman table definition into the derived format */
+    pub fn jpeg_make_c_derived_tbl(cinfo: j_compress_ptr,
+                    htbl: *mut JHUFF_TBL, pdtbl: *mut *mut c_derived_tbl);
 
     /* Generate an optimal table definition given the specified counts */
-    pub fn jpeg_gen_optimal_table(
-        cinfo: *mut j_compress_struct,
-        htbl: *mut JHUFF_TBL,
-        freq: *mut c_long,
-    );
-}
-
-// Opaque type stubs for structural coherence
-#[repr(C)]
-pub struct j_compress_struct {
-    _unused: [u8; 0],
-}
-
-#[repr(C)]
-pub struct JHUFF_TBL {
-    _unused: [u8; 0],
+    pub fn jpeg_gen_optimal_table(cinfo: j_compress_ptr,
+                    htbl: *mut JHUFF_TBL, freq: *mut c_long);
 }
